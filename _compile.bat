897b82c932160593fb7c8eda6f9e3e562164022b
Grigori Fursin <grigori.fursin@cTuning.org>
2016-09-15 21:18:51 +0200

    adding compile.bat

---
+++ _compile.bat
@@ -0,0 +1,11 @@
+call ck set env tags=compiler,lang-c,v19 --local --bat_file=tmp-ck-env.bat --bat_new --print && call tmp-ck-env.bat && del /Q tmp-ck-env.bat
+if %errorlevel% neq 0 exit /b %errorlevel%
+
+call ck set env tags=tool,cmake --local --bat_file=tmp-ck-env.bat --bat_new --print && call tmp-ck-env.bat && del /Q tmp-ck-env.bat
+if %errorlevel% neq 0 exit /b %errorlevel%
+
+mkdir build
+cd build
+cmake ..
+
+msbuild /p:Configuration=Release /p:Platform=Win32 CKNodeServer.sln
